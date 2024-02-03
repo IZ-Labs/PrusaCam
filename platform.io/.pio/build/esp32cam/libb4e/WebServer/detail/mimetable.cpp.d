230084b318026ee2a7f41cc8e4ce08a4f7849a9a
Jerry de Vos <devos.winsum@gmail.com>
2024-02-04 00:04:14 +0100

    longer timeout and default to psram

--- platform.io/.pio/build/esp32cam/libb4e/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32cam/libb4e/WebServer/detail/mimetable.cpp.o: \
- C:/Users/Jerry/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/Jerry/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/Jerry/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
--- platform.io/.pio/build/esp32cam/libc52/Micro-RTSP/JPEGSamples.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32cam/libc52/Micro-RTSP/JPEGSamples.cpp.o: \
- .pio/libdeps/esp32cam/Micro-RTSP/src/JPEGSamples.cpp \
- .pio/libdeps/esp32cam/Micro-RTSP/src/JPEGSamples.h
